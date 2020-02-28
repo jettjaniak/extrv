5d4b68b439c909a8d6d9faf24b02f0acabe17b5e
Kajetan Janiak <kajetan.janiak@gmail.com>
2020-02-28 12:36:19 +0100

    setup.py and examples

--- example.py
+++ example.py
@@ -1,27 +1,22 @@
-from wrappers.engine import *
-import matplotlib.pyplot as plt
+from extrv_engine import BondParameters, LigandType, ModelParameters, Settings, SimulationState
 
 
 if __name__ == '__main__':
-    p = ModelParameters(4.5, 0.01, 310, 0.05, 1e3, 5)
+    esel_bond_p = BondParameters(
+        bond_type=BondParameters.BondType.esel,
+        lambda_=77,
+        sigma=100,
+        k_f_0=0.06,
+        rec_dens=3600,
+        x1s=0.18,
+        k01s=2.6
+    )
+    psgl_lig_t = LigandType()
+    psgl_lig_t.add_bond_p(esel_bond_p)
+
+    p = ModelParameters(r_c=4.5, mu=0.01, temp=310, dens_diff=0.05, f_rep_0=1e3, tau=5)
     settings = Settings(p)
-    psgl_lig_p = LigandType()
-    esel_bond_p = BondParameters("esel", 77, 100, 0.06, 3600, 0.18, 2.6)
-    psgl_lig_p.add_bond_p(esel_bond_p)
-    settings.add_lig_type(psgl_lig_p, 20000)
+    settings.add_lig_type(psgl_lig_t, 10000)
 
-    # add seed parameter (positive integer) for reproducibility
-    ss = SimulationState(h_0=0.075, settings=settings)
-    sim_res = ss.simulate_with_history(n_steps=1e6, dt=1e-6, shear=0, save_every=100)
-
-    plt.subplot(211)
-    plt.plot(sim_res.rot)
-    plt.title("rotation")
-    plt.subplot(212)
-    plt.plot(sim_res.h)
-    plt.title("height")
-
-    # TODO: trajectory plot with individual bonds
-
-    plt.tight_layout()
-    plt.show()
+    s = SimulationState(h_0=0.075, settings=settings, seed=12345)
+    s.simulate_one_step(dt=1e-6, shear=100)
