1be070c85113298bb86eef9961ad9bc5f6ec7db3
Kajetan Janiak <kajetan.janiak@gmail.com>
2020-06-23 15:24:46 +0200

    numerical stability in position in Euler

--- engine/main.cpp
+++ engine/main.cpp
@@ -7,7 +7,18 @@
 #include <iostream>
 
 int main() {
-    for (int i = 220; i < 226; i++) {
+    array<uint32_t, 80> seeds {1731712957, 3190037783, 3759517182, 4177028560, 1830405957,
+                           3762189420, 1546919209, 752713666, 2277760481, 2225307367, 771879583, 2686016408, 3091179815,
+                           54641801, 822526847, 3485949959, 1808968316, 2740946722, 2128909877, 1735928740, 709544941,
+                           256163407, 2950738251, 67318337, 3705137511, 23055023, 3076940865, 2962143592, 3516257955,
+                           3987088566, 997047223, 3296440446, 3213720315, 501563541, 3609636494, 3947717566, 1205502233,
+                           3141042261, 2198693213, 1654518378, 8356930, 682264254, 1174718341, 556715953, 3943414791,
+                           3560480401, 1483454036, 1038552105, 325198726, 712250723, 783014756, 2310304458, 1895812771,
+                           2484051420, 622190755, 1641609963, 3466489807, 1441286071, 2419453959, 1533929822, 2464008272,
+                           4191568628, 1751682214, 2288410354, 3596124070, 1820690884, 2414023370, 2264240200, 3133142593,
+                           3102573477, 2513463839, 1460214160, 629244395, 2998528036, 1651710757, 156828413, 2688925137,
+                           1635904256, 1031466595, 2716957902};
+    for (auto seed : seeds) {
         Parameters p = Parameters(
                 4.5,
                 0.01,
@@ -29,19 +40,18 @@ int main() {
         );
         psgl.add_bond_type(&psgl_plus_esel_bond);
         p.add_ligands(&psgl, 20000);
-        unsigned int seed = i;
-        double dt = 1e-5;
-        auto s = EulerProbSS(0.03, &p, i, 1e-5);
+        double dt = pow(2, -23);
+        auto s = EulerProbSS(0.03, &p, seed, dt);
         double falling_time = 1.0;
         size_t max_steps_falling = falling_time / dt + 10;
-        double rolling_time = 5.0;
+        double rolling_time = 10.0;
         size_t max_steps_rolling = rolling_time / dt + 10;
         double max_time = falling_time + rolling_time;
 
         s.simulate(falling_time, max_steps_falling);
         std::cout << s.bd_lig_ind.size() << " bonds" << std::endl;
-        s.shear_rate = 100;
 
+        s.shear_rate = 10;
         s.simulate(max_time, max_steps_rolling);
         if (s.time < max_time)
             std::cout << "only " << s.time << " seconds done!" << std::endl;
--- engine/sources/EulerSS.cpp
+++ engine/sources/EulerSS.cpp
@@ -7,9 +7,66 @@ AbstrEulerSS::AbstrEulerSS(double dt) :
 void AbstrEulerSS::reset_stepper() {}
 
 double AbstrEulerSS::do_ode_step() {
-    namespace pl = std::placeholders;
-    auto rhs_system = std::bind(&AbstrSS::rhs, std::ref(*this), pl::_1 , pl::_2 , pl::_3);
-    stepper.do_step(rhs_system, pos, time, try_dt);
+    static array<double, 3> tiny_pos, dxdt;
+    rhs(pos,dxdt, time);
+    static double pos_item_temp, inc;
+    for (int i = 0; i < 3; i++) {
+        inc = tiny_pos[i] + dxdt[i] * try_dt;
+        if (inc != 0.0) {
+            pos_item_temp = pos[i] + inc;
+            // Good.
+            if (pos_item_temp != pos[i]) {
+                pos[i] = pos_item_temp;
+                tiny_pos[i] = 0.0;
+            // Not good.
+            } else tiny_pos[i] = inc;
+        }
+    }
+//    namespace pl = std::placeholders;
+//    auto rhs_system = std::bind(&AbstrSS::rhs, std::ref(*this), pl::_1 , pl::_2 , pl::_3);
+//
+//    static array<double, 3> pos_out, dxdt;
+//    stepper.do_step(rhs_system, pos, time, pos_out, try_dt);
+//
+//    vector<int> eq;
+//    for (int i = 0; i < 3; i++) {
+//        if (pos[i] == pos_out[i])
+//            eq.push_back(i);
+//    }
+//    static long n_ebdn = 0;
+//    if (!eq.empty()) {
+//        static bool eq_but_dxdt_nonzero = false;
+//        rhs(pos, dxdt, 0);
+//        for (const auto &eq_ind : eq) {
+//            if (dxdt[eq_ind] != 0) {
+//                eq_but_dxdt_nonzero = true;
+//                break;
+//            }
+//        }
+//
+//        if (eq_but_dxdt_nonzero) {
+//            n_ebdn++;
+//            std::cout << "< " << n_ebdn << "> " << std::endl;
+//            std::cout << "pos before: [";
+//            for (const auto &pos_item : pos) {
+//                std::cout << pos_item << ", ";
+//            }
+//            std::cout << "]" << std::endl;
+//
+//            std::cout << "pos after:  [";
+//            for (const auto &pos_item : pos_out) {
+//                std::cout << pos_item << ", ";
+//            }
+//            std::cout << "]" << std::endl;
+//
+//            std::cout << "dx / dt:    [";
+//            for (const auto &item : dxdt) {
+//                std::cout << item << ", ";
+//            }
+//            std::cout << "]" << std::endl << std::endl;
+//        }
+//    }
+//    pos = pos_out;
     double step_done_with_dt = try_dt;
     time += step_done_with_dt;  // do_step takes time by value
     try_dt = dt;
